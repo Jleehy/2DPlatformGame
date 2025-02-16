09bb93f3fe8bf00919d18360e97bbb37c4ea3f1c
seanhammell <sean.m.hammell@gmail.com>
2025-02-16 10:36:23 -0600

    Add jump/fall animations

--- 2d-platformer/player.gd
+++ 2d-platformer/player.gd
@@ -8,20 +8,38 @@ func _ready():
 	$AnimatedSprite2D.play()
 
 func _physics_process(delta):
+	# Reset the x velocity to 0 so the player
+	# only moves while holding the A or D key.
 	velocity.x = 0
+
+	# Move the player if the A or D key is being
+	# held.
 	if Input.is_action_pressed("move_left"):
 		velocity.x = -1 * speed
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
 
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "run"
+	# Update the player animation based on
+	# current velocity.
+	if velocity.x != 0 or velocity.y != 0:
 		$AnimatedSprite2D.flip_h = velocity.x < 0
+		if is_on_floor():
+			$AnimatedSprite2D.animation = "run"
+		else:
+			if velocity.y < 0:
+				$AnimatedSprite2D.animation = "jump"
+			else:
+				$AnimatedSprite2D.animation = "fall"
 	if velocity == Vector2.ZERO:
 		$AnimatedSprite2D.animation = "idle"
 
+	# Apply gravity.
 	velocity.y += gravity * delta
+
+	# Move the player.
 	move_and_slide()
 
+	# Add jump force if the jump button is
+	# pressed and the player is on the floor.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
