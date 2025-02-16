035aecd380fc5b24696b4bbcfea28ee66be3a7de
aepii <simentalma@yahoo.com>
2025-02-16 13:29:41 -0600

    Created singletons: GameManager, AudioManager, and InputManager

--- 2d-platformer/player.gd
+++ 2d-platformer/player.gd
@@ -1,26 +1,24 @@
 extends CharacterBody2D
 
-@export var speed = 600
-@export var jump_speed = -1200
-@export var gravity = 4000
+@export var speed: int = 600
+@export var jump_speed: int = -1200
+@export var gravity: int = 4000
 
-func _ready():
+func _ready() -> void:
 	$AnimatedSprite2D.play()
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	# Reset the x velocity to 0 so the player
 	# only moves while holding the A or D key.
 	velocity.x = 0
 
-	# Move the player if the A or D key is being
-	# held.
-	if Input.is_action_pressed("move_left"):
+	# Move the player if the A or D key is being held.
+	if InputManager.is_move_left_pressed():
 		velocity.x = -1 * speed
-	if Input.is_action_pressed("move_right"):
+	if InputManager.is_move_right_pressed():
 		velocity.x = speed
 
-	# Update the player animation based on
-	# current velocity.
+	# Update the player animation based on current velocity.
 	if velocity.x != 0 or velocity.y != 0:
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 		if is_on_floor():
@@ -39,7 +37,7 @@ func _physics_process(delta):
 	# Move the player.
 	move_and_slide()
 
-	# Add jump force if the jump button is
-	# pressed and the player is on the floor.
-	if Input.is_action_pressed("jump") and is_on_floor():
+	# Add jump force if the jump button is pressed and the player is on the floor.
+	if InputManager.is_jump_pressed() and is_on_floor():
 		velocity.y = jump_speed
+		#AudioManager.play_sound(jump_sound_here)  # Play jump sound
