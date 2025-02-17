e5aab295f7ddd05c633a7adfac758aade5421ce1
Zonefrog <43961208+Zonefrog@users.noreply.github.com>
2025-02-16 19:20:23 -0600

    new stuffs
    Here is the code.

---
+++ 2DPlatformGame-main/2DPlatformGame-main/2d-platformer/tile_map_layer.gd
@@ -0,0 +1,9 @@
+extends TileMapLayer
+
+# Example usage
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
