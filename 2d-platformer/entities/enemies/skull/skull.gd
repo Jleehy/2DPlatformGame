e0a6d3f363c3cdc2c28c959b3e7f1356c5688152
seanhammell <sean.m.hammell@gmail.com>
2025-04-13 18:22:42 -0500

    Add comments to skull.gd

--- 2d-platformer/entities/enemies/skull/skull.gd
+++ 2d-platformer/entities/enemies/skull/skull.gd
@@ -3,38 +3,44 @@ extends CharacterBody2D
 
 var bullet_scene: Resource = preload("res://entities/enemies/plant/plant_bullet.tscn")
 
-@export var max_health:int = 5
-@export var speed:int = 150
-@export var shot_speed:int = 100
-@export var chase_radius:int = 150
-@export var shot_radius:int = 300
+@export var max_health:int = 5      # Skull's health
+@export var speed:int = 150         # Skull's Speed
+@export var chase_radius:int = 150  # Radius where the Skull will chase the Player
+@export var shot_radius:int = 500   # Radius where the Skull will shoot at the Player
 
-var player_reference:CharacterBody2D
-var current_health:int
-var start_position:Vector2
-var shoot:bool = false
-var recenter:bool = false
-var is_dead:bool = false
+var player_reference:CharacterBody2D  # Player object
+var current_health:int                # Skull's current health
+var start_position:Vector2            # Skull's start position
+var shoot:bool = false                # Whether the Skull should shoot
+var recenter:bool = false             # Whether the Skull should recenter itself
+var is_dead:bool = false              # Whether the Skull is dead
 
 func _ready() -> void:
+	# Initialize the Skull
 	current_health = max_health
 	start_position = position
-	velocity = Vector2(speed, 0)
 	$AnimatedSprite2D.play("idle_ranged")
 
 
 func _process(delta:float) -> void:
+	# Calculate the Skull's distance from its center and to the Player
 	var distance_to_player = position.distance_to(player_reference.position)
 	var distance_to_center = position.distance_to(start_position)
+
 	if distance_to_player < chase_radius and distance_to_center < chase_radius and !recenter:
+		# If the Player is inside the chase radius, the Skull hasn't moved too far from the
+		# center, and the Skull is not recentering, chase the player.
 		shoot = false
 		velocity.x = speed * (player_reference.position.x - position.x) / distance_to_player
 		velocity.y = speed * (player_reference.position.y - position.y) / distance_to_player
 		if $AnimatedSprite2D.animation == "idle_ranged":
 			$AnimatedSprite2D.play("idle_melee")
 	elif distance_to_player < shot_radius and !recenter:
+		# If the Player is inside the shot radius and the Skull is not recentering, shoot at
+		# the Player.
 		shoot = true
 	elif distance_to_center > speed / 20:
+		# If the skull is too far from the starting position, move back.
 		shoot = false
 		recenter = true
 		velocity.x = speed * (start_position.x - position.x) / distance_to_center
@@ -42,11 +48,13 @@ func _process(delta:float) -> void:
 		if $AnimatedSprite2D.animation == "idle_melee":
 			$AnimatedSprite2D.play("idle_ranged")
 	else:
+		# Sit and wait at the starting position.
 		recenter = false
 		velocity = Vector2.ZERO
 
 	move_and_slide()
 
+	# Deal damage to the Player on contact.
 	for i in get_slide_collision_count():
 		var collider = get_slide_collision(i)
 		if collider.get_collider().is_in_group("player"):
@@ -59,6 +67,7 @@ func _on_hit_area_body_entered(body: Node2D) -> void:
 		body.bounce()
 
 		if $AnimatedSprite2D.animation == "idle_melee":
+			# Recenter the Skull if the Player deals damage.
 			$AnimatedSprite2D.play("hit_melee")
 			recenter = true
 
@@ -75,12 +84,14 @@ func _on_hit_area_body_entered(body: Node2D) -> void:
 
 func _on_animated_sprite_2d_animation_looped() -> void:
 	if $AnimatedSprite2D.animation == "idle_ranged" and shoot and !recenter:
+		# Shoot a bullet at the Player.
 		var bullet:Variant = bullet_scene.instantiate()
 		var x_dir:float = (player_reference.position.x - position.x) / position.distance_to(player_reference.position)
 		var y_dir:float = (player_reference.position.y - position.y) / position.distance_to(player_reference.position)
 		bullet.set_direction(Vector2(x_dir, y_dir))
 		add_child(bullet)
 	if $AnimatedSprite2D.animation == "hit_melee" or $AnimatedSprite2D.animation == "hit_ranged":
+		# Switch to the idle ranged animation after a hit animation.
 		$AnimatedSprite2D.animation = "idle_ranged"
 
 
